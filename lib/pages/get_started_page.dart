8652ba9f9bc07993a47129f3e2fdb20da6efd32b
Ripat-2345 <ripatalsafar@gmail.com>
2024-04-20 10:12:41 +0700

    fix: re-create get_started_screen

--- lib/pages/get_started_page.dart
+++ lib/pages/get_started_page.dart
@@ -1,95 +1,149 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:lodging_app/common_widgets/custom_filled_button_widget.dart';
 import 'package:lodging_app/theme.dart';
 
-class GetStartedPage extends StatelessWidget {
+class GetStartedPage extends StatefulWidget {
   const GetStartedPage({super.key});
+
+  @override
+  State<GetStartedPage> createState() => _GetStartedPageState();
+}
+
+class _GetStartedPageState extends State<GetStartedPage> {
+  CarouselController carouselController = CarouselController();
+  int _currentIndex = 0;
+
+  final List _dataGetStarted = [
+    [
+      "assets/images/illustration2.png",
+      "Mulailah petualangan Anda dengan mencari tempat menginap yang sempurna sesuai kebutuhan dan preferensi Anda."
+    ],
+    [
+      "assets/images/illustration3.png",
+      "Temukan penginapan impian Anda dengan mudah. Cukup masukkan lokasi, tanggal, dan preferensi Anda untuk menemukan pilihan terbaik."
+    ],
+    [
+      "assets/images/illustration4.png",
+      "Jelajahi berbagai pilihan penginapan yang nyaman dan terjangkau dengan mudah di aplikasi kami. Mulailah perjalanan tak terlupakan Anda sekarang!"
+    ],
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/background.png"),
-            fit: BoxFit.cover,
-          ),
-        ),
+      backgroundColor: darkBlueColor,
+      body: Center(
         child: Container(
           width: double.infinity,
-          height: double.infinity,
-          color: darkBlueColor.withOpacity(0.6),
+          height: 650,
+          margin: const EdgeInsets.symmetric(horizontal: 22, vertical: 60),
+          padding: const EdgeInsets.only(
+            left: 20,
+            top: 40,
+            right: 20,
+            bottom: 30,
+          ),
+          decoration: BoxDecoration(
+            color: whiteColor,
+            borderRadius: BorderRadius.circular(10),
+          ),
           child: Column(
             children: [
-              const SizedBox(height: 100),
               Container(
-                width: 250,
-                height: 60,
+                width: 160,
+                height: 40,
                 padding: const EdgeInsets.symmetric(
-                  horizontal: 26,
-                  vertical: 13,
+                  horizontal: 18,
+                  vertical: 8,
                 ),
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(100),
                   color: whiteColor,
+                  borderRadius: BorderRadius.circular(100),
+                  border: Border.all(
+                    color: blueColor,
+                    width: 1.5,
+                  ),
                 ),
                 child: Center(
                   child: Image.asset(
-                    'assets/images/logo.png',
-                    width: 188,
-                    height: 34,
-                    fit: BoxFit.contain,
+                    "assets/images/logo.png",
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
-              const Spacer(),
-              Container(
-                margin: const EdgeInsets.only(bottom: 25, right: 12, left: 12),
-                width: 346,
-                height: 235,
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  gradient: LinearGradient(
-                    colors: [
-                      Colors.white.withOpacity(0.9),
-                      Colors.white.withOpacity(0.8),
-                    ],
-                    begin: AlignmentDirectional.topStart,
-                    end: AlignmentDirectional.bottomEnd,
+              CarouselSlider(
+                items: _dataGetStarted.map((data) {
+                  return Image.asset(
+                    data[0],
+                    width: 318,
+                    height: 219,
+                  );
+                }).toList(),
+                options: CarouselOptions(
+                  height: 318,
+                  viewportFraction: 1,
+                  enableInfiniteScroll: false,
+                  autoPlay: false,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _currentIndex = index;
+                    });
+                  },
+                ),
+                carouselController: carouselController,
+              ),
+              SizedBox(
+                height: 84,
+                child: Center(
+                  child: Text(
+                    _dataGetStarted[_currentIndex][1],
+                    style: semiBoldTextStyle.copyWith(color: darkBlueColor),
+                    maxLines: 5,
+                    textAlign: TextAlign.center,
                   ),
                 ),
-                child: Column(
-                  children: [
-                    Text(
-                      "Let's Get Started",
-                      style: semiBoldTextStyle.copyWith(
-                        color: darkBlueColor,
-                        fontSize: 28,
-                      ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: _dataGetStarted.map((data) {
+                  return Container(
+                    height: 12,
+                    width: 12,
+                    margin: const EdgeInsets.only(
+                      right: 10,
                     ),
-                    const SizedBox(height: 20),
-                    Text(
-                      'We have a lot of lodging that you use not only for staying but growing.',
-                      style: mediumTextStyle.copyWith(
-                        color: blueColor,
-                        fontSize: 16,
-                        wordSpacing: 1,
-                        letterSpacing: 1,
-                      ),
-                      textAlign: TextAlign.center,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: _currentIndex == _dataGetStarted.indexOf(data)
+                          ? yellowColor
+                          : blueColor,
                     ),
-                    const Spacer(),
-                    Center(
-                      child: CustomFilledButtonWidget(
-                        buttonTitle: "Explore Lodging",
+                  );
+                }).toList(),
+              ),
+              const Spacer(),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: _currentIndex == 2
+                    ? CustomFilledButtonWidget(
+                        buttonTitle: "Start Journey!",
                         onPressed: () {
-                          Navigator.pushReplacementNamed(context, '/login');
+                          Navigator.pushNamedAndRemoveUntil(
+                            context,
+                            '/login',
+                            (route) => false,
+                          );
+                        },
+                      )
+                    : CustomFilledButtonWidget(
+                        buttonTitle: "Next",
+                        width: 160,
+                        onPressed: () {
+                          carouselController.nextPage();
                         },
                       ),
-                    ),
-                  ],
-                ),
               ),
             ],
           ),
